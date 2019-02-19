0eef9568896584b20927a8bb60ae62eddc6eb764
Vladieboy <vlad.tikunov@gmail.com>
2019-02-18 19:55:27 -0800

    successfully tested api

---
+++ TestDeathSolution/TestDeathSolution.Web/Controllers/api/UserAPIController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestDeathSolution.Web.Controllers.api
+{
+    [RoutePrefix("api")]
+    public class UserAPIController : ApiController
+    {
+        [Route, HttpGet]
+        public HttpResponseMessage Hello()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "suckuss");
+        }
+    }
+}
